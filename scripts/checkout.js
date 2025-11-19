e34bc9f507f1877de741afd655e2f9f61da2d44b
Poya Aghajani <poyakisom1381@gmail.com>
2025-11-19 19:17:04 +0330

    delete product from card

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { cart } from "../data/cart.js";
+import { cart, removeFromCard } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -41,7 +41,8 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
             Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary 
+            js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
             </span>
         </div>
@@ -92,3 +93,13 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummeryHtml;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+
+    removeFromCard(productId);
+
+    console.log(cart);
+  });
+});
