3eb596d891d7a5aaaf66640d77c71ee71c3f541a
Poya Aghajani <poyakisom1381@gmail.com>
2025-11-28 19:35:48 +0330

    create oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,89 @@
+function Cart(localStorageKey) {
+  const cart = {
+    cartItems: undefined,
+    loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+
+      if (!this.cartItems) {
+        this.cartItems = [
+          {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2,
+            deliveryOptionId: "1",
+          },
+          {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1,
+            deliveryOptionId: "2",
+          },
+        ];
+      }
+    },
+    saveToStorage() {
+      localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
+    },
+    addToCart(productId) {
+      let matchingItem;
+
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        this.cartItems.push({
+          productId: productId,
+          quantity: 1,
+          deliveryOptionId: "1",
+        });
+      }
+
+      this.saveToStorage();
+    },
+    removeFromCard(productId) {
+      const newCart = [];
+
+      this.cartItems.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+          newCart.push(cartItem);
+        }
+      });
+
+      this.cartItems = newCart;
+
+      this.saveToStorage();
+    },
+    getCartQuantity() {
+      return this.cartItems.reduce((sum, item) => sum + item.quantity, 0);
+    },
+    updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem;
+
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      matchingItem.deliveryOptionId = deliveryOptionId;
+
+      this.saveToStorage();
+    },
+  };
+
+  return cart;
+}
+
+const cart = Cart("cart-oop");
+
+const bussinesCart = Cart("cart-bussines");
+
+cart.loadFromStorage();
+
+bussinesCart.loadFromStorage();
+
+console.log(cart);
+console.log(bussinesCart);
